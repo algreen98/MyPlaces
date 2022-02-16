d8b6e621e0f83a9bd02d259cc5a7dbb3349db8ad
algreen98 <75377340+algreen98@users.noreply.github.com>
2022-02-16 17:08:49 +0300

    Add files via upload
    start

---
+++ MyPlaces/MyPlaces/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Александр Бровкин  on 16.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
